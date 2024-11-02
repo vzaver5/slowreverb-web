05eef24ec57ec76c6c8a480dc104b9d3280d4133
vzaver5 <42894180+vzaver5@users.noreply.github.com>
2024-11-02 13:09:36 -0400

    Change text style for title
    Changed text style for title to Honk (Google Font),
    Added support for Framer Motion to allow Title to have "wave" motion

--- react-frontend/App.js
+++ react-frontend/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DragNDrop from "./dnd/DragNDrop"
+import { motion } from "framer-motion";
 
 function App() {
     const [data, setData] = useState(null);
@@ -17,11 +18,27 @@ function App() {
             });
     }, []);
     
-  return (
-      <div>
-          Slow and Reverb Generator
-          {data ? <h1>{data.message}</h1> : <h1>Loading...</h1>}
-          <DragNDrop onFilesSelected={setData} width='700px' height='400px' />
+    return (
+        <div>
+            <div className="Title" style={{ display: "flex" }}>
+                {"Slow And Reverb Generator".split("").map((char, index) => (
+                <motion.span
+                    key={index}
+                    initial={{ y: 0, x: 0, opacity: .5}}
+                    animate={{ y: 40, x: 30, opacity: 1 }}
+                    transition={{
+                        delay: index * .1,
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "mirror"
+                    }}
+                    style={{ display: "inline-block" }}
+                >
+                    {char}
+                </motion.span>
+                ))}
+            </div>
+            <DragNDrop onFilesSelected={setData} width='1000px' height='200px' />
       </div>
   );
 }
